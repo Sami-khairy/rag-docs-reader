ee436fc28c13ea6d144c7830fd9fa0cf628dbc34
Sami-Khairy <samikhairy2003@gmail.com>
2025-05-06 14:53:34 +0100

    using spring ai

---
+++ src/main/java/ma/khairy/ragdocsreader/ingestion_service/SpringAi.java
@@ -0,0 +1,37 @@
+package ma.khairy.ragdocsreader.ingestion_service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringAi implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/docs/pddf.pdf")
+    private Resource marketPDF;
+
+    public SpringAi(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var pdfReader = new ParagraphPdfDocumentReader(marketPDF);
+        TextSplitter textSplitter = new TokenTextSplitter();
+        vectorStore.accept(textSplitter.apply(pdfReader.get()));
+        log.info("VectorStore Loaded from sprin ai with data!");
+    }
+}
+
+
+
