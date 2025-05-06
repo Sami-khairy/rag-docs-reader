4cc291e71df714aeab50154afaa3052e83d7364c
Sami-Khairy <samikhairy2003@gmail.com>
2025-05-06 14:02:37 +0100

    init project

---
+++ src/main/java/ma/khairy/ragdocsreader/RagDocsReaderApplication.java
@@ -0,0 +1,13 @@
+package ma.khairy.ragdocsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagDocsReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagDocsReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/khairy/ragdocsreader/RagDocsReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.khairy.ragdocsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagDocsReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
