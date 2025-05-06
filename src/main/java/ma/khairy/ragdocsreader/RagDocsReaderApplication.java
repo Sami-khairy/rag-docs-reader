11b1b3eb9a04b88d20ed13b24a54776e9bef1ec8
Sami-Khairy <samikhairy2003@gmail.com>
2025-05-06 15:12:33 +0100

    using manual config

--- src/main/java/ma/khairy/ragdocsreader/RagDocsReaderApplication.java
+++ src/main/java/ma/khairy/ragdocsreader/RagDocsReaderApplication.java
@@ -1,7 +1,15 @@
 package ma.khairy.ragdocsreader;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgDistanceType.COSINE_DISTANCE;
+import static org.springframework.ai.vectorstore.pgvector.PgVectorStore.PgIndexType.HNSW;
 
 @SpringBootApplication
 public class RagDocsReaderApplication {
@@ -10,4 +18,17 @@ public class RagDocsReaderApplication {
         SpringApplication.run(RagDocsReaderApplication.class, args);
     }
 
+    //    pgvector manual config
+    @Bean
+    public VectorStore myVectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .dimensions(1536)                    // Optional: defaults to model dimensions or 1536
+                .distanceType(COSINE_DISTANCE)       // Optional: defaults to COSINE_DISTANCE
+                .indexType(HNSW)                     // Optional: defaults to HNSW
+                .initializeSchema(true)              // Optional: defaults to false
+                .schemaName("public")                // Optional: defaults to "public"
+                .vectorTableName("myTable")     // Optional: defaults to "vector_store"
+                .maxDocumentBatchSize(10000)         // Optional: defaults to 10000
+                .build();
+    }
 }
--- src/main/java/ma/khairy/ragdocsreader/ingestion_service/SpringAi.java
+++ src/main/java/ma/khairy/ragdocsreader/ingestion_service/SpringAi.java
@@ -6,6 +6,7 @@ import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.Resource;
@@ -20,7 +21,7 @@ public class SpringAi implements CommandLineRunner {
     @Value("classpath:/docs/pddf.pdf")
     private Resource marketPDF;
 
-    public SpringAi(VectorStore vectorStore) {
+    public SpringAi(@Qualifier("myVectorStore") VectorStore vectorStore) {
         this.vectorStore = vectorStore;
     }
 
